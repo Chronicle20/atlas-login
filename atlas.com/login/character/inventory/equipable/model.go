e94c0dcc818e50da64e207a289c160c1228db047
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-27 16:24:23 -0400

    Add Equipable attributes.

--- atlas.com/login/character/inventory/equipable/model.go
+++ atlas.com/login/character/inventory/equipable/model.go
@@ -1,25 +1,38 @@
 package equipable
 
+import "time"
+
 type Model struct {
-	id            uint32
-	itemId        uint32
-	slot          int16
-	strength      uint16
-	dexterity     uint16
-	intelligence  uint16
-	luck          uint16
-	hp            uint16
-	mp            uint16
-	weaponAttack  uint16
-	magicAttack   uint16
-	weaponDefense uint16
-	magicDefense  uint16
-	accuracy      uint16
-	avoidability  uint16
-	hands         uint16
-	speed         uint16
-	jump          uint16
-	slots         uint16
+	id             uint32
+	itemId         uint32
+	slot           int16
+	strength       uint16
+	dexterity      uint16
+	intelligence   uint16
+	luck           uint16
+	hp             uint16
+	mp             uint16
+	weaponAttack   uint16
+	magicAttack    uint16
+	weaponDefense  uint16
+	magicDefense   uint16
+	accuracy       uint16
+	avoidability   uint16
+	hands          uint16
+	speed          uint16
+	jump           uint16
+	slots          uint16
+	ownerName      string
+	locked         bool
+	spikes         bool
+	karmaUsed      bool
+	cold           bool
+	canBeTraded    bool
+	levelType      byte
+	level          byte
+	experience     uint32
+	hammersApplied uint32
+	expiration     time.Time
 }
 
 func (m Model) ItemId() uint32 {
